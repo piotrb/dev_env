f6980d27c5bd0462bb965d5088cc3f22851a49aa
Piotr Banasik <piotr.banasik@gmail.com>
2014-04-05 14:50:07 -0700

    tweaks

--- .profile.d/dev_env.sh
+++ .profile.d/dev_env.sh
@@ -1,40 +1,35 @@
-#export EDITOR="$HOME/bin/mate_editor"
 export EDITOR="vim"
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
-#export CC=/usr/bin/gcc-4.2 # needed for Lion only
-
 export MYSQL_UNIX_PORT=/tmp/mysql.sock
 
-alias cucumber="bundle exec cucumber"
-
-alias rspec="bundle exec rspec"
-
+alias cucumber="spring_wrapper cucumber"
+alias rspec="spring_wrapper rspec"
 alias ph="bundle exec ph"
-
-alias r="rspec"
-
-alias dl="~/Work/dev_launcher/run.sh"
-
 alias cap="bundle exec cap"
+alias rake="spring_wrapper rake"
+alias rails="spring_wrapper rails"
+
+export RBENV_GEMSETS="global"
 
 export SPEC_OPTS=--color
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-function load_rvm() {
-  # This loads RVM into a shell session.
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-  # Add RVM to PATH for scripting
-  export PATH="$PATH:$HOME/.rvm/bin"
-}
-
 export PATH=$PATH:~/Library/Python/2.7/bin
+export PYTHONPATH=/usr/local/lib/python2.6/site-packages
 
 ulimit -n 2048
 
+function dev_gemfile() {
+  export BUNDLER_GEMFILE=Gemfile.dev
+  ln -s ~/.Gemfile.dev Gemfile.dev
+}
 
+function reg_gemfile() {
+  export BUNDLER_GEMFILE=Gemfile
+}
 
