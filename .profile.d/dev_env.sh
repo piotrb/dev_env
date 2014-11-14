215e21fa851bcd5bf3fef0bf195beabdc24d6206
Piotr Banasik <piotr.banasik@gmail.com>
2014-11-14 12:14:06 +0000

    Fixes for linux

--- .profile.d/dev_env.sh
+++ .profile.d/dev_env.sh
@@ -1,5 +1,4 @@
 export EDITOR="vim"
-export GIT_EDITOR="mate -w"
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
--- .profile.d/powerline.zsh
+++ .profile.d/powerline.zsh
@@ -5,12 +5,13 @@ if [ -e ~/.python/lib/Python2.7/site-packages/powerline ]; then
   export PATH=$HOME/.python/bin:$PATH
 fi
 
+# linux
 if [ -e ~/.local/lib/python2.7/site-packages/powerline ]; then
-  POWERLINE_BASE=$HOME/.local/lib/python2.7/site-packages/powerline
+  export POWERLINE_BASE=$HOME/.local/lib/python2.7/site-packages/powerline
   export PATH=$HOME/.local/bin:$PATH
 fi
 
 if [ -e $POWERLINE_BASE ]; then
   . $POWERLINE_BASE/bindings/zsh/powerline.zsh
-  powerline-daemon
-fi  
+  powerline-daemon -q
+fi
